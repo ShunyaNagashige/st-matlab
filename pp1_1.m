7f385e807bbbf1bf0e5ec337a112e848f98f5f0a
ShunyaNagashige <nagashige190441091@gmail.com>
2021-09-16 21:00:19 +0900

    [fix] 課題1-1のグラフを描画

--- pp1_1.m
+++ pp1_1.m
@@ -1,1 +1,12 @@
 clc; clear all;
+time_length = 2;
+fc=1;
+fs=100;
+t=0:(fs*time_length);t=t./fs;
+
+sin_wave=sin(2*pi*t+pi/2);
+
+plot(t,sin_wave,'LineWidth',2);grid on;
+xlabel('t [s]');
+ylabel('x(t)');
+
